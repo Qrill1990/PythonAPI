a79aab07cdc2de75c1ad9ced5afbe7c5d63cdd52
Kirill Lyubushkin <lyubushkin.k@gmail.com>
2022-01-19 16:41:18 +0300

    added test to check if file exists

---
+++ tests/test_json_file_folder.py
@@ -0,0 +1,28 @@
+from lib.base_case import get_sid
+import allure
+from lib.my_requests import MyRequests
+from lib import variables
+from lib import payloads
+from lib.assertions import Assertions
+import os.path
+
+
+@allure.epic("Integration Cases")
+class TestJsonFileFolder:
+    @allure.title("Тест на ответ на проверку, что json файл выгрузился")
+    @allure.description("This test checks if json file exists")
+    def test_check_json_file(self):
+        sid = get_sid()
+        response = MyRequests.post(url=f"{variables.callback_url}",
+                                   json=payloads.payload_callback_200_person_data(sid=sid),
+                                   headers=payloads.common_json_headers
+                                   )
+        Assertions.assert_code_status(response, 200)
+
+        response1 = MyRequests.post(url=f"{variables.url}/questionnaire/fill", headers=payloads.common_json_headers,
+                                    json=payloads.payload_questionnaire_200(payloads.unique_uuid))
+
+        Assertions.assert_code_status(response1, 200)
+
+        assert os.path.isfile(f'/Users/kirilllyubushkin/Downloads/{sid}.json'), "There is no file in directory"
+
